5e81cfdd4e2a6608a867d666006105cf53719fc1
Администратор <troewiha@gmail.com>
2016-04-24 17:06:15 +0300

    Initial commit

---
+++ src/main/java/com/springapp/config/Config.java
@@ -0,0 +1,39 @@
+package com.springapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by Администратор on 24.04.2016.
+ */
+
+@Configuration
+@ComponentScan(basePackages = "com.springapp")
+@EnableWebMvc
+public class Config extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Администратор
+  Date: 24.04.2016
+  Time: 13:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Ok</title>
+</head>
+<body>
+<h1>Hello World!!!</h1>
+</body>
+</html>
